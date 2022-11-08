809772a633459cf74756b35511dc1905efd5679e
Mateusz Kaflowski <mkaflowski@gmail.com>
2022-11-08 14:18:50 +0100

    first commit

---
+++ app/src/main/java/sanity/wearsportstopwatch/MainActivity.kt
@@ -0,0 +1,161 @@
+package sanity.wearsportstopwatch
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.Bundle
+import android.os.IBinder
+import android.os.Vibrator
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
+import sanity.wearsportstopwatch.databinding.ActivityMainBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val time = System.currentTimeMillis()
+    var startTime = System.currentTimeMillis() / 1000
+    val lapTime = 3
+    var isRunning = false
+    var isRunningCurrent = false
+    var mMediaPlayer = MediaPlayer()
+    lateinit var timeTv: TextView
+    lateinit var startBt: Button
+
+    private lateinit var mService: MyService
+    private var mBound: Boolean = false
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as MyService.LocalBinder
+            mService = binder.getService()
+            mBound = true
+            isRunning = mService.isRunning
+            if (isRunning) {
+                startBt.text = "STOP"
+                startBt.setBackgroundColor(Color.parseColor("#ff0000"))
+            }
+            Log.e("Test", "BIND")
+            startUpdateThread(timeTv)
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+            Log.e("Test", "UNBIND")
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        isRunningCurrent = true
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        timeTv = findViewById(R.id.time)
+        val currentTv = findViewById<TextView>(R.id.currentTimeTextView)
+        startBt = findViewById(R.id.buttonStart)
+
+        val minutes = findViewById<TextView>(R.id.minutes)
+
+        val plus = findViewById<Button>(R.id.plus)
+        plus.setOnClickListener(View.OnClickListener {
+            minutes.text = (Integer.parseInt(minutes.text.toString()) + 1).toString()
+        })
+
+        val minus = findViewById<Button>(R.id.minus)
+        minus.setOnClickListener(View.OnClickListener {
+            minutes.text = (Integer.parseInt(minutes.text.toString()) - 1).toString()
+        })
+
+
+        timeTv.text = "STOP"
+
+        startBt.setOnClickListener(View.OnClickListener {
+            if (!isRunning) {
+//                startTime = System.currentTimeMillis() / 1000
+//                isRunning = true
+//                startThread(timeTv, currentTv)
+                val intent = Intent(this, MyService::class.java)
+                intent.putExtra("MINUTES", Integer.parseInt(minutes.text.toString()))
+                applicationContext.startForegroundService(intent)
+                isRunning = true
+
+                startUpdateThread(timeTv)
+
+                startBt.text = "STOP"
+                startBt.setBackgroundColor(Color.parseColor("#ff0000"))
+
+            } else {
+                isRunning = false
+                timeTv.text = "STOP"
+                mService.stopThread()
+                startBt.text = "START"
+                startBt.setBackgroundColor(Color.parseColor("#4CAF50"))
+            }
+        })
+
+        Thread {
+            while (isRunningCurrent) {
+                currentTv.post {
+                    val current = LocalDateTime.now()
+
+                    val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+                    val formatted = current.format(formatter)
+                    currentTv.text = formatted
+                }
+                Thread.sleep(250)
+            }
+        }.start()
+    }
+
+    private fun startUpdateThread(timeTv: TextView) {
+        Thread {
+            while (isRunning) {
+                timeTv.post {
+                    val formatted = mService.getFormattedTime()
+                    timeTv.text = formatted
+                }
+                Thread.sleep(250)
+            }
+        }.start()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Bind to LocalService
+        Intent(this, MyService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        mBound = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunningCurrent = false
+    }
+
+}
---
+++ app/src/main/java/sanity/wearsportstopwatch/MyService.kt
@@ -0,0 +1,141 @@
+package sanity.wearsportstopwatch
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+import android.os.Vibrator
+import android.util.Log
+import androidx.core.app.NotificationManagerCompat
+
+class MyService : Service() {
+    private val ONGOING_NOTIFICATION_ID = 128
+
+    var isRunning = false
+    var mMediaPlayer = MediaPlayer()
+    var startTime = System.currentTimeMillis() / 1000
+    var lapTime = 10
+
+    // Binder given to clients
+    private val binder = LocalBinder()
+
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MyService = this@MyService
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+
+        lapTime = 60 * intent.getIntExtra("MINUTES",5)
+        createChannel(this)
+        startThread()
+        return super.onStartCommand(intent, flags, startId)
+
+    }
+
+    private fun startForeground() {
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(
+                    this, 0, notificationIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+
+        val notification: Notification = Notification.Builder(this, "CHANNEL_TIMER")
+            .setContentTitle("Timer")
+            .setContentText("Preparing...")
+            .setSmallIcon(androidx.legacy.v4.R.drawable.notification_bg)
+            .setContentIntent(pendingIntent)
+            .setTicker("Ticker")
+            .build()
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
+    }
+
+    fun stopThread() {
+        isRunning = false
+        stopForeground(true)
+    }
+
+    fun startThread() {
+        startForeground()
+        isRunning = false
+        Thread.sleep(1100)
+        startTime = System.currentTimeMillis() / 1000
+        isRunning = true
+        Thread(Runnable {
+            while (isRunning) {
+                val time = lapTime + (startTime - System.currentTimeMillis() / 1000)
+                val timeString = (time / 60).toString() + " : " + (time % 60).toString()
+                Log.d("time", timeString)
+
+
+                val pendingIntent: PendingIntent =
+                    Intent(this, MainActivity::class.java).let { notificationIntent ->
+                        PendingIntent.getActivity(
+                            this, 0, notificationIntent,
+                            PendingIntent.FLAG_IMMUTABLE
+                        )
+                    }
+                val notification: Notification = Notification.Builder(this, "CHANNEL_TIMER")
+                    .setContentTitle(timeString)
+                    .setContentText("Timer")
+                    .setSmallIcon(androidx.legacy.v4.R.drawable.notification_bg)
+                    .setContentIntent(pendingIntent)
+                    .setTicker("Ticker")
+                    .build()
+
+                val mNotifyMgr = NotificationManagerCompat.from(applicationContext)
+                mNotifyMgr.notify(ONGOING_NOTIFICATION_ID, notification)
+
+                if (time <= 0) {
+                    val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+                    val vibrationPattern = longArrayOf(0, 300, 50, 500)
+                    //-1 - don't repeat
+                    //-1 - don't repeat
+                    val indexInPatternToRepeat = -1
+                    vibrator.vibrate(vibrationPattern, indexInPatternToRepeat)
+
+                    mMediaPlayer.release()
+                    mMediaPlayer = MediaPlayer.create(this, R.raw.noti)
+                    mMediaPlayer.isLooping = false
+                    mMediaPlayer.start()
+
+                    startTime = System.currentTimeMillis() / 1000
+
+                    continue
+                }
+                Thread.sleep(1000)
+            }
+        }).start()
+    }
+
+    private fun createChannel(context: Context) {
+        val mNotificationManager =
+            context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        // The id of the channel.
+        // The user-visible name of the channel.
+        val name: CharSequence = "Timer"
+        // The user-visible description of the channel.
+        val description: String = "Timer"
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val mChannel = NotificationChannel("CHANNEL_TIMER", name, importance)
+        // Configure the notification channel.
+        mChannel.description = description
+        mChannel.setShowBadge(true)
+        mChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        mNotificationManager.createNotificationChannel(mChannel)
+    }
+
+    fun getFormattedTime(): CharSequence? {
+        val time = lapTime + (startTime - System.currentTimeMillis() / 1000)
+        return (time / 60).toString() + " : " + (time % 60).toString();
+    }
+}
